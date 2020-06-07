567a11109b29cf144b9c7983d8fabb97357f651e
mtkozlowski <kooziu@gmail.com>
2020-06-07 10:16:58 +0200

    Management WIP

---
+++ src/manage/manage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Main from '../styles/Templates/Main';
+import RegularSection from '../styles/Templates/RegularSection';
+
+const Manage = () => (
+
+// Fetch all entries with Errors
+
+    <Main>
+        <RegularSection>
+            <h1>Manage!</h1>
+        </RegularSection>
+    </Main>
+)
+
+export default Manage;
