749f31668bd870782998a0f079942cd5c353664c
mtkozlowski <kooziu@gmail.com>
2020-12-09 14:58:08 +0100

    [refact] remove data from code

--- src/config/youtubeKey.js
+++
@@ -1,1 +0,0 @@
-export default "AIzaSyDG4psFuUW5Kdl2bskiu2LiPXhgorRPCq0";
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,69 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Main from '../styles/Templates/Main';
 import RegularSection from '../styles/Templates/RegularSection';
-import ResultList from '../styles/Organisms/ResultList';
-import SearchForm from '../styles/Molecules/SearchForm';
-import SearchHelper from '../styles/Molecules/SearchHelper';
-import recognizeSiglum from '../tasks/recognizeSiglum';
 
-class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      formTextValue: '',
-      serverResponse: [],
-      recognizedSiglum: {},
-    };
-  }
+const Home = () => {
+  const [randomVerse, setRandomVerse] = useState({
+    content: '',
+    sigla: [],
+  });
 
-  handleChange(data) {
-    let recognized = '';
-    try {
-      recognized = recognizeSiglum(data);
-    } catch (err) {
-      console.log(err);
-    }
-    this.setState({
-      formTextValue: data,
-      recognizedSiglum: recognized,
-    });
-  }
+  useEffect(() => {
+    const data = fetch(process.env.REACT_APP_VERSES_API);
 
-  handleSubmit() {
-    this.callServer();
-  }
+    data
+      .then((response) => {
+        return response.json();
+      })
+      .then((items) => {
+        const { content: newContent, sigla: newSigla } = items;
+        setRandomVerse({
+          content: newContent,
+          sigla: newSigla,
+        });
+      });
+  }, []);
 
-  callServer() {
-    const { formTextValue } = this.state;
-    if (formTextValue === '') {
-      return;
-    }
-
-    const url = new URL('http://localhost:8000/search');
-    const params = { query: formTextValue };
-
-    url.search = new URLSearchParams(params).toString();
-
-    fetch(url)
-      .then((res) => res.json())
-      .then((res) => this.setState({ serverResponse: res }));
-  }
-
-  render() {
-    const { recognizedSiglum, serverResponse } = this.state;
-    return (
-      <Main>
-        <RegularSection>
-          <SearchForm
-            onFormSubmit={this.handleSubmit}
-            onFormValueChange={this.handleChange}
-          />
-          <SearchHelper recognizedSiglum={recognizedSiglum} />
-          <ResultList cardsData={serverResponse} />
-        </RegularSection>
-      </Main>
-    );
-  }
-}
+  return (
+    <Main>
+      <RegularSection>
+        <p>{randomVerse.content}</p>
+        <p>{randomVerse.sigla}</p>
+      </RegularSection>
+    </Main>
+  );
+};
 
 export default Home;
