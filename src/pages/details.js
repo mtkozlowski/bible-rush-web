fae5ad26f36f7f1c304cbe5f6d50d474263b01a4
mtkozlowski <kooziu@gmail.com>
2020-12-09 15:05:35 +0100

    [style] details and ResourceCard

--- src/pages/details.js
+++ src/pages/details.js
@@ -1,33 +1,39 @@
-import React, { useEffect, useState } from "react";
-import Main from "../styles/Templates/Main";
-import RegularSelection from "../styles/Templates/RegularSection";
+import React, { useEffect, useState } from 'react';
+import { PropTypes } from 'prop-types';
+
+import Main from '../styles/Templates/Main';
+import RegularSelection from '../styles/Templates/RegularSection';
 
 function Details({ match }) {
+  const [item, setItem] = useState({});
+
   useEffect(() => {
     const fetchItem = async () => {
-      const url = new URL("http://localhost:8000/details");
+      const url = new URL('http://localhost:8000/details');
       const params = { query: match.params.id };
 
       url.search = new URLSearchParams(params).toString();
       const data = await fetch(url);
-      const item = await data.json();
-      setItem(...item);
+      const newItem = await data.json();
+      setItem(...newItem);
     };
 
     fetchItem();
   }, [match]);
 
-  const [item, setItem] = useState({});
-
   return (
     <Main>
       <RegularSelection>
         <h1>Details!</h1>
-        <img src={item.thumbnailsSlug} alt="thumbnailsSlug" />
-        {item.title}
+        <img src={`${item.thumbnailsSlug}sddefault.jpg`} alt="thumbnailsSlug" />
+        <h2>{item.title}</h2>
       </RegularSelection>
     </Main>
   );
 }
+
+Details.propTypes = {
+  match: PropTypes.objectOf(PropTypes.object()).isRequired,
+};
 
 export default Details;
--- src/styles/Molecules/ResourceCard.js
+++ src/styles/Molecules/ResourceCard.js
@@ -1,32 +1,52 @@
-import React from "react";
-import styled from "styled-components";
-import { H3, H4 } from "../Atoms/Headings";
-import { Link } from "react-router-dom";
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { PropTypes } from 'prop-types';
+import { H3, H4 } from '../Atoms/Headings';
 
 const StyledCard = styled.div`
   display: flex;
+  & + & {
+    margin-top: 1.6rem;
+  }
 `;
 
-const ImageWrapper = styled.div`
-  height: 138px;
-  width: 246px;
+const ImageWrapper = styled(Link)`
+  display: block;
+  height: 180px;
+  width: 320px;
+  margin-right: 1.6rem;
+  order: 0;
 `;
+
+const StyledWrapper = styled.div`
+  order: 1;
+`;
+
 const ResourceCard = ({ cardData }) => (
   <StyledCard>
-    <ImageWrapper>
-      <img src={cardData.thumbnailsSlug} alt="thumbnailsSlug" />
-    </ImageWrapper>
-    <div>
-      <H3>{cardData.title}</H3>
+    <StyledWrapper>
+      <H3>
+        <Link to={`/details/${cardData.id}`}>{cardData.title}</Link>
+      </H3>
       <H4>Sigle</H4>
       <ul>
         <li>Dz 25, 13-21</li>
         <li>Dz 28, 16-20. 30-31</li>
         <li>J 21, 20-25</li>
       </ul>
-      <Link to={`/details/${cardData.id}`}>Go to DETAILS</Link>
-    </div>
+    </StyledWrapper>
+    <ImageWrapper to={`/details/${cardData.id}`}>
+      <img
+        src={`${cardData.thumbnailsSlug}mqdefault.jpg`}
+        alt="thumbnailsSlug"
+      />
+    </ImageWrapper>
   </StyledCard>
 );
+
+ResourceCard.propTypes = {
+  cardData: PropTypes.objectOf(PropTypes.object()).isRequired,
+};
 
 export default ResourceCard;
