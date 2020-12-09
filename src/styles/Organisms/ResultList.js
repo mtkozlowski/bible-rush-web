bcf3df969f1e48e037e6f2224011c162f5e71d63
mtkozlowski <kooziu@gmail.com>
2020-12-09 15:16:24 +0100

    [refact] ResultList

--- src/styles/Organisms/ResultList.js
+++ src/styles/Organisms/ResultList.js
@@ -1,13 +1,18 @@
-import React from "react";
-import RegularSection from "../Templates/RegularSection";
-import ResourceCard from "../Molecules/ResourceCard";
+import React from 'react';
+import { PropTypes } from 'prop-types';
+import RegularSection from '../Templates/RegularSection';
+import ResourceCard from '../Molecules/ResourceCard';
 
 const ResultList = ({ cardsData }) => (
   <RegularSection>
     {cardsData.map((r) => (
-      <ResourceCard cardData={r} />
+      <ResourceCard cardData={r} key={r.id} />
     ))}
   </RegularSection>
 );
+
+ResultList.propTypes = {
+  cardsData: PropTypes.objectOf(PropTypes.object()).isRequired,
+};
 
 export default ResultList;
